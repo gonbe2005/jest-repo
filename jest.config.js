15638f99a8debaef879927f57c315f99f2eb789f
田中雄也 <tanaka.yuya002@kk.jp.panasonic.com>
2023-09-16 12:23:19 +0900

    test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  testEnvironment: 'jest-environment-node',  // 'node'から'jest-environment-node'に変更
+  testEnvironment: 'jest-environment-node',  
   moduleFileExtensions: ['js', 'mjs'],
   testMatch: ['**/?(*.)+(spec|test).mjs'],
   transform: {
-    '^.+\\.mjs$': 'babel-jest'  // .mjsファイルをBabelでトランスパイルするように設定
-  }
+    '^.+\\.mjs$': 'babel-jest'
+  },
+  collectCoverage: true  // カバレッジ収集を有効にする
 };
