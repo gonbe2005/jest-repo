849a63111950e288a5d981f009821943a346770c
田中雄也 <tanaka.yuya002@kk.jp.panasonic.com>
2023-09-17 11:34:17 +0900

    test

--- src/module1/function1.mjs
+++ src/module1/function1.mjs
@@ -1,13 +1,13 @@
-export function add(a, b) {
-    return a + b;
-  }
+export const handler = async (event) => {
+  const { num1, num2 } = event;
   
-  export const handler = async (event) => {
-    const { num1, num2 } = event;
-    const result = add(num1, num2);
-    return {
+  if (num1 === null || num2 === null || num1 === undefined || num2 === undefined) {
+      throw new Error("Invalid input");
+  }
+
+  const result = add(num1, num2);
+  return {
       statusCode: 200,
       body: JSON.stringify({ result }),
-    };
   };
-  
+};
