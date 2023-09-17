787c8c4fb3b6c4e1317d54bef8ac757f640c08ec
田中雄也 <tanaka.yuya002@kk.jp.panasonic.com>
2023-09-17 11:26:09 +0900

    test

--- src/module1/function1.test.mjs
+++ src/module1/function1.test.mjs
@@ -1,17 +1,55 @@
 import { handler } from './function1.mjs';
 
-describe('function1 Lambda handler tests', () => {
-  it('should add two numbers correctly', async () => {
-    const event = {
-      num1: 2,
-      num2: 3
-    };
+describe('function1 handlerのテスト', () => {
+    it('2つの正の数の加算が正しく行われる', async () => {
+        const response = await handler({ num1: 5, num2: 3 });
+        expect(response.statusCode).toBe(200);
+        expect(JSON.parse(response.body).result).toBe(8);
+    });
 
-    const response = await handler(event);
-    
-    const responseBody = JSON.parse(response.body);
+    it('負の数と正の数の加算が正しく行われる', async () => {
+        const response = await handler({ num1: -5, num2: 3 });
+        expect(response.statusCode).toBe(200);
+        expect(JSON.parse(response.body).result).toBe(-2);
+    });
 
-    expect(response.statusCode).toBe(200);
-    expect(responseBody.result).toBe(5);
-  });
+    it('正の数と負の数の加算が正しく行われる', async () => {
+        const response = await handler({ num1: 5, num2: -3 });
+        expect(response.statusCode).toBe(200);
+        expect(JSON.parse(response.body).result).toBe(2);
+    });
+
+    it('2つの負の数の加算が正しく行われる', async () => {
+        const response = await handler({ num1: -5, num2: -3 });
+        expect(response.statusCode).toBe(200);
+        expect(JSON.parse(response.body).result).toBe(-8);
+    });
+
+    it('num1がゼロの場合の処理が正しく行われる', async () => {
+        const response = await handler({ num1: 0, num2: 3 });
+        expect(response.statusCode).toBe(200);
+        expect(JSON.parse(response.body).result).toBe(3);
+    });
+
+    it('num2がゼロの場合の処理が正しく行われる', async () => {
+        const response = await handler({ num1: 5, num2: 0 });
+        expect(response.statusCode).toBe(200);
+        expect(JSON.parse(response.body).result).toBe(5);
+    });
+
+    it('num1がnullの場合の処理', async () => {
+        await expect(handler({ num1: null, num2: 3 })).rejects.toThrow();
+    });
+
+    it('num2がnullの場合の処理', async () => {
+        await expect(handler({ num1: 5, num2: null })).rejects.toThrow();
+    });
+
+    it('num1がundefinedの場合の処理', async () => {
+        await expect(handler({ num2: 3 })).rejects.toThrow();
+    });
+
+    it('num2がundefinedの場合の処理', async () => {
+        await expect(handler({ num1: 5 })).rejects.toThrow();
+    });
 });
