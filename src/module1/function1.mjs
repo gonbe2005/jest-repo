41bc1acd74fa2a17b34c272665a509c07a422710
田中雄也 <tanaka.yuya002@kk.jp.panasonic.com>
2023-09-17 11:38:51 +0900

    test

--- src/module1/function1.mjs
+++ src/module1/function1.mjs
@@ -1,6 +1,10 @@
+export function add(a, b) {
+  return a + b;
+}
+
 export const handler = async (event) => {
   const { num1, num2 } = event;
-  
+
   if (num1 === null || num2 === null || num1 === undefined || num2 === undefined) {
       throw new Error("Invalid input");
   }
