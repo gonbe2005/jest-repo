894ba5e1b23825466b7959525b5c7a19498d7bbd
tanaka-yuya <gonbe2005@gmail.com>
2023-10-25 11:03:25 +0900

    CICD

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
-  region = var.region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region      = "ap-northeast-3"
+  access_key  = getenv("AWS_ACCESS_KEY_ID")
+  secret_key  = getenv("AWS_SECRET_ACCESS_KEY")
 }
 
 module "api_gateway" {
