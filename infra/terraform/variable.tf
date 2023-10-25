887171d1ac046559b29e059bc987beb175ccd459
tanaka-yuya <gonbe2005@gmail.com>
2023-10-25 11:08:08 +0900

    CICD

--- infra/terraform/variable.tf
+++ infra/terraform/variable.tf
@@ -10,11 +10,13 @@ variable "account_id" {
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS Access Key ID"
   type        = string
+  default     = ""
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS Secret Access Key"
   type        = string
+  default     = ""
 }
 
 variable "region" {
